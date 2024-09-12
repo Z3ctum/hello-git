328f56aeec5c695f31a87f04264f7d4a1c83691e
Z3ctum <jespinosap1500@alumno.ipn.mx>
2024-09-11 23:29:03 -0600

    Se realizo un Hola mundo con una funcion

--- hellogit2.rs
+++ hellogit2.rs
@@ -1,3 +1,8 @@
 fn main(){
-    println!("Hello git since Rust version 2.2.2.2.2.2");
+    println!("Hello git since Rust version 2.2.2.2.2.2.3.4");
+    hola_mundo_desde_rust();
+}
+
+fn hola_mundo_desde_rust(){
+    println!("\n\tHola Mundo desde Rust con una funcion");
 }
